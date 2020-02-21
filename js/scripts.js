f56ae842fee4210d0268b8646b3ea47e9ad6e271
q1ncy <qbarnabas@gmail.com>
2020-02-21 13:00:01 +0300

    add what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
         $("#id2").toggle()
         $("#id1").toggle()
     })
+    $(".iconTwo").click(function(){
+        $("#id4").toggle()
+        $("#id3").toggle()
+    })
+    $(".iconThree").click(function(){
+        $("#id6").toggle()
+        $("#id5").toggle()
+    })
 })
